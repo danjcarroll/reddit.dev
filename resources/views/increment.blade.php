90ef33d7c2f4bddcc2aa218050d8f33375eb32e1
primalrambler <primal.rambler@gmail.com>
2016-10-11 10:12:50 -0500

    modified into blade format

---
+++ resources/views/increment.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('head')
+	<title>Increment</title>
+@stop
+
+@section('content')
+	<div class="title">NOTCH IT</div>
+	<h1>you typed..</h1>
+	<div class="title">{{ $number }}</div>
+	<h1>WE NOTCHED IT TO...</h1>
+	<div class="title">{{ $number + 1 }}</div>
+
+@stop
+
