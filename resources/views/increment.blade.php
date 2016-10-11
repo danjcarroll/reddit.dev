529fb7c6be08532b333e6d56780f914f79fb2353
primalrambler <primal.rambler@gmail.com>
2016-10-11 11:19:40 -0500

    minor formatting clean up

--- resources/views/increment.blade.php
+++ resources/views/increment.blade.php
@@ -9,7 +9,7 @@
 	<h1>you typed..</h1>
 	<div class="title">{{ $number }}</div>
 	<h1>WE NOTCHED IT TO...</h1>
-	<div class="title">{{ $number + 1 }}</div>
+	<div class="title">{{ $incremented }}</div>
 
 @stop
 
