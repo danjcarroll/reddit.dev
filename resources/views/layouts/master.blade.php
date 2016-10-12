ba1b31a6247f520478b2cc98bbe0756260ffa302
primalrambler <primal.rambler@gmail.com>
2016-10-12 11:06:21 -0500

    added a section for top and bottom scripts and footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,9 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        @yield('top-scripts')
+        
         <style>
             html, body {
                 height: 100%;
@@ -34,11 +37,16 @@
                 font-size: 96px;
             }ß
         </style>
+
+        @yield('head')
+
     </head>
     <body>
         <div class="container">
             <div class="content">
+
                 @yield('content')
+
             </div>
         </div>
 
@@ -47,5 +55,8 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         {{-- jQuery --}}
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
+        
+        @yield('bottom-scripts')
+
     </body>
 </html>
